46a69293c9c6a548c9a09a438ddc6508dfb29d08
Manisha Rana <manisharana02005@gmail.com>
2026-09-12 18:12:59 +0530

    update operation

--- Backend/day3-crud/server.js
+++ Backend/day3-crud/server.js
@@ -43,16 +43,28 @@ app.get('/datas', (req, res)=>{
 });
 
 
-
 //delete
 app.delete('/delete/:id', (req, res)=>{
     //let idx=req.params; //we'll get an object, we'll get the id from the api
-    //console.log(idx);// -> { id: '3' } -> http://localhost:3000/delete/3
+    //console.log(idx);// -> { id: '3' } -> http://localhost:3000/delete/3 -> 3 is the params here -> if you pass anything from the parameter it will come in string only -> 3 is also a string
 
     let { id } = req.params; //destructuring id becuase object is coming inside params
     let userData = data.filter((val)=>val.id!==id);
     data=userData;
     res.send("user deleted successfully");
+});
+
+
+//update
+app.put('/update/:id', (req, res)=>{
+
+    let { id } = req.params;
+    let { name } = req.body; //we are taking name from the body
+
+    let updatedData = data.map((val)=>
+        val.id===id ? {...val, name } : val //update name and keep other values as it is
+    );
+    res.send(updatedData);
 });
 
 
